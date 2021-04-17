4d369d2ef07ce4e0ab6359251c45d9be96367cba
DJWickWack <35749854+DJWickWack@users.noreply.github.com>
2021-04-17 14:47:03 -0400

    Added Main Menu and Start up Text

---
+++ CycleBreakers/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Begin(){
+        SceneManager.LoadScene("TheCycle", LoadSceneMode.Single);
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
