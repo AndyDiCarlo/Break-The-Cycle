b2359ab4cf513b39658e76d3abe4177b7b057e13
Andy DiCarlo <andy01880@gmail.com>
2021-04-18 23:51:32 -0400

    Set of Enemies spawn on each room enter
    No enemies in first room though but spawn every room after including when player returns to first room

--- CycleBreakers/Assets/Scripts/EnemyFactory.cs
+++ CycleBreakers/Assets/Scripts/EnemyFactory.cs
@@ -7,21 +7,43 @@ public class EnemyFactory : IFactory
 
     private Enemy rangedEnemy;
     private Enemy meleeEnemy;
-    private int currentWave;
+    public int currentRoom;
 
     public EnemyFactory(Enemy ranged, Enemy melee)
     {
         this.rangedEnemy = ranged;
         this.meleeEnemy = melee;
-        currentWave = 0;
+        currentRoom = 0;
     }
 
     public IProduct produce()
     {
         float meleeChance = 0.5f;
-        //where to spawn
-        float x = Random.Range(-8.1f, -7.2f);
-        float y = Random.Range(3f, 3.9f);
+        float x=-8f, y=3.1f;
+
+        currentRoom = GameObject.Find("Player").GetComponent<Player>().roomNumber;
+
+        //where to spawn (room 1-4 in order)
+        if (currentRoom == 0)
+        {
+            x = Random.Range(-8.2f, -7.75f);
+            y = Random.Range(3f, 3.9f);
+        }
+        if(currentRoom == 1)
+        {
+            x = Random.Range(-8.2f, -7.3f);
+            y = Random.Range(.75f, 1f);
+        }
+        if (currentRoom == 2)
+        {
+            x = Random.Range(-3.9f, -3.6f);
+            y = Random.Range(.75f, 1.6f);
+        }
+        if (currentRoom == 3)
+        {
+            x = Random.Range(-4.6f, -3.6f);
+            y = Random.Range(3.65f, 3.9f);
+        }
 
         float rand = Random.Range(0f, 1f);
 
@@ -40,5 +62,9 @@ public class EnemyFactory : IFactory
         return Object.Instantiate(prefab, new Vector3(x,y,0f), Quaternion.identity);
     }
 
+    public void setCurrentRoom(int roomNum)
+    {
+        currentRoom = roomNum;
+    }
     
 }
